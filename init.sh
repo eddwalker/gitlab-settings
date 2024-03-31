376cf768efcb30988e65c0850dcd8caf32ac5022
Edd Walker <eddwalker@tutanota.com>
2024-03-31 20:38:01 +0100

    Install missed software

--- init.sh
+++ init.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Replace this server ssh port to let gitlab server to expose port 22 
+# Replace this server ssh port to let gitlab server to expose port 22
 sudo sed -E -i'' 's|(#)?Port.*|Port=10022|' /etc/ssh/sshd_config
 systemctl restart sshd
 
 # Install required software
 apt update
-apt install docker-compose docker-buildx --yes
+apt install docker-compose docker-buildx jq --yes
 
 # Run every docker container from this repo
 for i in $(find . -type f -name up.sh); do
